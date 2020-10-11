3e3b0611148c15ce5828985661d979e31dde9a19
Steve McDougall <juststeveking@gmail.com>
2020-10-11 20:34:33 +0100

    Organising and craeting a fromString

--- src/Uri.php
+++ src/Uri.php
@@ -6,19 +6,57 @@ use JustSteveKing\ParameterBag\ParameterBag;
 
 class Uri
 {
-    private ?string $path;
-
-    private string $host;
-
+    /**
+     * @var string
+     */
     private string $scheme;
 
+    /**
+     * @var string
+     */
+    private string $host;
+
+    /**
+     * @var string|null
+     */
+    private ?string $path;
+
+    /**
+     * @var ParameterBag
+     */
     private ParameterBag $query;
 
+    /**
+     * @return self
+     */
     public static function build(): self
     {
         return new self();
     }
 
+    /**
+     * @param string $uri
+     * @return self
+     */
+    public static function fromString(string $uri): self
+    {
+        $uri = parse_url($uri);
+
+        if (! is_array($uri)) {
+            throw new \RuntimeException("URI failed to parse using parse_url, please ensure is valid URL.");
+        }
+
+        return (new self())
+            ->addScheme($uri['scheme'])
+            ->addHost($uri['host'])
+            ->addPath($uri['path'])
+            ->addQuery($uri['query']);
+    }
+
+    /**
+     * @param string $scheme
+     * @return self
+     */
     public function addScheme(string $scheme): self
     {
         $this->scheme = $scheme;
@@ -26,6 +64,10 @@ class Uri
         return $this;
     }
 
+    /**
+     * @param string $host
+     * @return self
+     */
     public function addHost(string $host): self
     {
         $this->host = $host;
@@ -33,13 +75,25 @@ class Uri
         return $this;
     }
 
-    public function addPath(string $path): self
+    /**
+     * @param string|null $path
+     * @return self
+     */
+    public function addPath(?string $path): self
     {
+        if (is_null($path)) {
+            return $this;
+        }
+
         $this->path = (substr($path, 0, 1) === '/') ? $path : "/{$path}";
 
         return $this;
     }
 
+    /**
+     * @param string $path
+     * @return self
+     */
     public function addQuery(string $path): self
     {
         $this->query = ParameterBag::fromString($path);
